120b84d45ece0686bcd46f663f93fed2c2b1380c
Teleswagz <teleswagz@live.com>
2016-12-21 00:43:48 -0500

    new files

---
+++ Crowns.java
@@ -0,0 +1,18 @@
+public class Crowns {
+  
+  public static void printer(int num) {
+    
+    System.out.println(num);
+    
+  }
+  
+  
+  
+}
+                    
+
+    
+    
+                      
+                   
+  
---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    
+    Scanner scan = new Scanner(System.in);
+    
+    System.out.println("Enter your current clan's crown count.");
+    
+    int crown_count = scan.nextInt();
+    
+    printer(crown_count);
+    
+    
+    
+    
+  }
+  
+}
