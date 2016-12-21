fc1bf4d0b57064b77fb8809f48f5335d588badb1
Teleswagz <teleswagz@live.com>
2016-12-21 01:35:26 -0500

    edit

--- Crowns.java
+++ Crowns.java
@@ -1,9 +1,26 @@
 public class Crowns {
   
-  public static void printer(int num) {
+  public static int printer(int crown_count, int chest_level) {
     
-    System.out.println(num);
+    int counter = 1;
+    int total_crowns = 0;
+    int current_crowns = 0;
+    int crowns_needed = 0;
     
+    while( counter <= 10) {
+      counter += 1;
+      total_crowns += 50 * counter;
+    }
+    counter = 0;
+    
+    while (counter <= chest_level) {
+      counter += 1;
+      current_crowns += 50 * counter;
+    }
+    
+    crowns_needed = total_crowns - current_crowns;
+    
+    return crowns_needed;
   }
   
   
--- Main.java
+++ Main.java
@@ -5,15 +5,16 @@ public class Main {
     
     Scanner scan = new Scanner(System.in);
     
-    System.out.println("Enter your current clan's crown count.");
-    
+    System.out.println("Enter your clan's current chest level.");
+    int chest_level = scan.nextInt();
+    System.out.println("Enter your clan's current crown count in that level.");
     int crown_count = scan.nextInt();
     
-    printer(crown_count);
-    
-    
-    
-    
+    int result = Crowns.printer(crown_count, chest_level);
+                       
+                       
+    System.out.println("Your clan needs "+result+" more crowns to get level 10.");
+                      
   }
   
 }
